0eac300f87943b83b92d18f59537d73ff8799f1c
willian33 <altoconsuma@yahoo.com>
2024-01-12 01:01:36 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+//tipo de dado
+// booleanos 
+
+// conversao implicita  
+
+const numero = 456;
+const numeroString = Number ("456");
+
+//number 
+//string
+
+console.log (numero + numeroString);
+
+// conversao explicita
+
---
+++ truthy-falsy.js
@@ -0,0 +1,25 @@
+// boolean 
+
+//const usuarioLogado = true ;
+//const contaPaga = false ;
+
+// truthy ou falsy 
+
+// 0 => false 
+//  => true 
+
+//console.log( 0 == false);
+//console.log("" == false);
+//console.log( 1 == true);
+
+//null
+//undefined 
+
+let minhaVar;
+let varNull = null
+
+let numero = 1;
+let texto = "Alura"
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
